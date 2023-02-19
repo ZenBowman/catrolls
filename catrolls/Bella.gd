5641fe43e6e0742e3af86179e5efbbc6ee85a314
ZenBowman <PrasanSamtani@gmail.com>
2023-02-19 15:28:03 -0800

    Add files via upload

---
+++ catrolls/Bella.gd
@@ -0,0 +1,11 @@
+extends Sprite
+
+func _init():
+	print("Come here, mommy!")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+
---
+++ catrolls/BellaBody2D.gd
@@ -0,0 +1,25 @@
+extends KinematicBody2D
+
+var speed = 350
+var angular_speed = PI
+
+onready var animated_sprite = $AnimatedSprite
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Make Bella rotate every frame.
+func _process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("ui_left"):
+		velocity -= Vector2.RIGHT
+		animated_sprite.play("walking")
+	elif Input.is_action_pressed("ui_right"):
+		velocity += Vector2.RIGHT
+		animated_sprite.play("walking")
+	else:
+		animated_sprite.play("default")
+		animated_sprite.stop()
+	position += (speed * velocity) * delta
