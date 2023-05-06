23758f09f30ee3f6b61c351acd7db6bf0ad8bf49
ZenBowman <PrasanSamtani@gmail.com>
2023-05-06 16:40:36 -0700

    Diagonal jumping added

--- catrolls/Snowy.gd
+++ catrolls/Snowy.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 
 var speed: int = 350
+var gravityspeed: int = 0
 var angular_speed: int = PI
 onready var animated_sprite: AnimatedSprite = $AnimatedSprite
 
@@ -9,34 +10,50 @@ enum BellaState {
 	JUMPING
 }
 
-var current_state = BellaState.GROUNDED
+var current_state = BellaState.JUMPING
 var velocity: Vector2 = Vector2.ZERO
 var screen_size: Vector2
+var timer = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
+	timer = Timer.new()
+	add_child(timer)
+
+	timer.connect("timeout", self, "every_second")
+	timer.set_wait_time(5.0)
+	timer.set_one_shot(false) # Make sure it loops
+	timer.start()
+	
+	velocity = Vector2.DOWN
+
+func every_second():
+	print(position.y)
 
 func clamp_to_screen(someposition: Vector2):
-	print("Before clamping: ")
-	print(someposition.y)
-	someposition.x = clamp(someposition.x, 20, screen_size.x-20)
-	someposition.y = clamp(someposition.y, 100, screen_size.y-100)
-	print("After clamping: ")
-	print(someposition.y)
+	someposition.x = clamp(someposition.x, 0, screen_size.x-20)
+	someposition.y = clamp(someposition.y, -1000, 50)
 	return someposition
 
 func process_grounded(delta):
 	velocity = Vector2.ZERO
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("ui_up"):
+		if Input.is_action_pressed("ui_right"):
+			velocity = (3*Vector2.UP) + Vector2.RIGHT
+			speed = 150
+		elif Input.is_action_pressed("ui_left"):
+			velocity = (3*Vector2.UP) + Vector2.LEFT
+			speed = 150
+		else:
+			velocity = Vector2.UP
+		current_state = BellaState.JUMPING
+	elif Input.is_action_pressed("ui_left"):
 		velocity -= Vector2.RIGHT
 		#animated_sprite.play("walkingleft")
 	elif Input.is_action_pressed("ui_right"):
 		velocity += Vector2.RIGHT
 		#animated_sprite.play("walkingright")
-	elif Input.is_action_pressed("ui_up"):
-		velocity = Vector2.UP
-		current_state = BellaState.JUMPING
 	else:
 		velocity = Vector2.ZERO
 		animated_sprite.play("default")
@@ -46,11 +63,12 @@ func process_grounded(delta):
 
 func process_jumping(delta):
 	#animated_sprite.play("jumping")
-	speed -= 3 	# gravity
-	position += (speed * velocity) * delta
+	gravityspeed += 3 	# gravity
+	position += ((gravityspeed * Vector2.DOWN) + (speed * velocity)) * delta
 	position = clamp_to_screen(position)
-	if (position.y >= (screen_size.y-100)):
+	if (position.y >= 50):
 		speed = 350
+		gravityspeed = 0
 		current_state = BellaState.GROUNDED
 
 func _process(delta):
@@ -58,4 +76,3 @@ func _process(delta):
 		process_grounded(delta)
 	elif (current_state == BellaState.JUMPING):
 		process_jumping(delta)
-	
