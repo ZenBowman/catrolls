63e5a2187a0e6922024ca4783519c9c784a17d2f
ZenBowman <PrasanSamtani@gmail.com>
2023-05-06 20:54:06 -0700

    added obstacles

--- catrolls/scripts/Snowy.gd
+++ catrolls/scripts/Snowy.gd
@@ -1,19 +1,31 @@
 extends KinematicBody2D
 
-var speed: int = 350
+var speed: int = 502
 var gravityspeed: int = 0
 var angular_speed: int = PI
+var groomtimer: float = 0.0
+var on_obstacle: Area2D = null
 onready var animated_sprite: AnimatedSprite = $AnimatedSprite
 
 enum BellaState {
 	GROUNDED,
-	JUMPING
+	JUMPING,
+	GROOMING
 }
 
 var current_state = BellaState.JUMPING
 var velocity: Vector2 = Vector2.ZERO
 var screen_size: Vector2
 var timer = null
+
+func collided(obstacle: Area2D):
+	if (BellaState.JUMPING):
+		if position.y <= obstacle.position.y:
+			change_state_grounded()
+			on_obstacle = obstacle
+		else:
+			gravityspeed += 200
+	
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -27,6 +39,7 @@ func _ready():
 	timer.start()
 	
 	velocity = Vector2.DOWN
+	animated_sprite.play("default")
 
 func every_second():
 	pass
@@ -34,7 +47,7 @@ func every_second():
 
 func clamp_to_screen(someposition: Vector2):
 	someposition.x = clamp(someposition.x, 0, screen_size.x-20)
-	someposition.y = clamp(someposition.y, -1000, 50)
+	someposition.y = clamp(someposition.y, -1600, 50)
 	return someposition
 
 func process_grounded(delta):
@@ -51,29 +64,51 @@ func process_grounded(delta):
 		current_state = BellaState.JUMPING
 	elif Input.is_action_pressed("ui_left"):
 		velocity -= Vector2.RIGHT
-		#animated_sprite.play("walkingleft")
+		animated_sprite.play("runningleft")
 	elif Input.is_action_pressed("ui_right"):
 		velocity += Vector2.RIGHT
-		#animated_sprite.play("walkingright")
+		animated_sprite.play("runningright")
+	elif Input.is_action_pressed("ui_accept"):
+		groomtimer = 0.0
+		current_state = BellaState.GROOMING
+		animated_sprite.play("grooming")
 	else:
 		velocity = Vector2.ZERO
 		animated_sprite.play("default")
 		animated_sprite.stop()
 	position += (speed * velocity) * delta
+	if (on_obstacle != null):
+		# Check if we have fallen off the obstacle
+		if (on_obstacle.get_overlapping_areas().find(self) == -1):
+			velocity = Vector2.ZERO
+			current_state = BellaState.JUMPING
+			on_obstacle = null
 	position = clamp_to_screen(position)
 
+func change_state_grounded():
+	speed = 500
+	gravityspeed = 0
+	current_state = BellaState.GROUNDED
+
 func process_jumping(delta):
-	#animated_sprite.play("jumping")
+	animated_sprite.play("jumping")
 	gravityspeed += 3 	# gravity
 	position += ((gravityspeed * Vector2.DOWN) + (speed * velocity)) * delta
 	position = clamp_to_screen(position)
 	if (position.y >= 50):
-		speed = 350
-		gravityspeed = 0
+		change_state_grounded()	
+
+func process_grooming(delta):
+	groomtimer += delta	
+	if (groomtimer > 2.0):
+		animated_sprite.play("default")
 		current_state = BellaState.GROUNDED
+		groomtimer = 0.0
 
 func _process(delta):
 	if (current_state == BellaState.GROUNDED):
 		process_grounded(delta)
 	elif (current_state == BellaState.JUMPING):
 		process_jumping(delta)
+	elif (current_state == BellaState.GROOMING):
+		process_grooming(delta)
--- catrolls/scripts/SnowyScene1.gd
+++ catrolls/scripts/SnowyScene1.gd
@@ -1,7 +1,12 @@
-extends Node2D
+extends 	Node2D
 
 onready var cat: KinematicBody2D = $Snowy
 onready var door: Area2D = $Door
+onready var obstacle1: Area2D = $obstacle1
+onready var obstacle2: Area2D = $obstacle2
+onready var obstacle3: Area2D = $obstacle3
+
+onready var obstacles = [obstacle1, obstacle2, obstacle3]
 
 var deltasum: float = 0.0
 
@@ -12,11 +17,16 @@ func _ready():
 func check_if_goal_reached():
 	if (door.get_overlapping_bodies().find(cat) != -1):
 		get_tree().change_scene("res://Scene1Complete.tscn")
-		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	check_if_goal_reached()
+	check_collisions()
+	
+func check_collisions():
+	for obstacle in obstacles:
+		if (obstacle.get_overlapping_bodies().find(cat) != -1):
+			cat.call("collided", obstacle)
 
 func _on_Button_pressed():
 	get_tree().change_scene("res://ChooseYourCharacter.tscn")
