ea88f33f5695abadb6a10ba58bd808e0cf0eff85
ZenBowman <PrasanSamtani@gmail.com>
2024-05-27 07:24:02 -0700

    Add ability to kill coyotes in scene 3

--- catrolls/SnowyScene3.gd
+++ catrolls/SnowyScene3.gd
@@ -2,24 +2,28 @@ extends Node2D
 
 var timer: float = 0.0
 var coyotespeed = 100.0
+
 onready var kitten = $Snowy
-onready var kittenBB = $Snowy/CollisionShape2D
+onready var kittenArea = $Snowy/Area2D
+onready var kittenBB = $Snowy/Area2D/CollisionShape2D
+
 onready var coyote1 = $Coyote1
 onready var coyote1ProgressBar = $Coyote1/Coyote1/ProgressBar
+onready var coyote1Collision = $Coyote1/Area2D
+
 onready var coyote2 = $Coyote2
 onready var coyoteTrigger = $CoyoteTrigger
+
+signal coyote1_dead
+
 enum SceneState {
 	STARTED_SCENE,
 	POOPED,
 	COYOTE_FIGHT,
+	COYOTE2_FIGHT
 }
 
 var state = SceneState.STARTED_SCENE
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 
 func _on_snowy_attack():
 	pass
@@ -35,6 +39,7 @@ func process_coyotefight(delta):
 
 func process_pooped(delta):
 	if (coyoteTrigger.get_overlapping_bodies().find(kitten) != -1):
+		print("starting fight")
 		state = SceneState.COYOTE_FIGHT
 
 func process_started_scene(delta):
@@ -57,9 +62,14 @@ func _process(delta):
 
 
 func _on_Snowy_snowy_attack():
-	coyote1ProgressBar.value -= 1
-	var r = range_lerp(coyote1ProgressBar.value, 10, 100, 1, 0)
-	var g = range_lerp(coyote1ProgressBar.value, 10, 100, 0, 1)
-	
-	print("Snowy attacked me!")
+	if (coyote1Collision.overlaps_area(kittenArea)):
+		coyote1ProgressBar.value -= 1
+		var r = range_lerp(coyote1ProgressBar.value, 10, 100, 1, 0)
+		var g = range_lerp(coyote1ProgressBar.value, 10, 100, 0, 1)
+	if (coyote1ProgressBar.value == 0):
+		state = SceneState.COYOTE2_FIGHT
+		emit_signal("coyote1_dead")
+
+
+func _on_SnowyScene3_coyote1_dead():
 	pass # Replace with function body.
